da492e61fd2b5ac30b64097b3dd8ceb1389aea03
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-04-15 06:07:01 +1000

    Display text in alt screen buffer

--- main.c
+++ main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
+#define ESC "\x1B"
+#define CSI ESC "["
+
+static void setupconsole(void) {
+  puts(CSI "?1049h");
+}
+
+static void resetconsole(void) {
+  puts(CSI "?1049l");
+}
+
+static void edit(char *text) {
+  puts(CSI "0;0H");
+  puts(CSI "0J");
+  printf("%s", text);
+}
+
 int main(void) {
-  puts("Untitled text adventure game");
-  puts("----------------------------");
-  puts("By the UTAS Programming Club");
-  putchar('\n');
-  puts("Currently unimplemented :(");
-  
+  setupconsole();
+  edit("Untitled text adventure game\n"
+       "----------------------------\n"
+       "By the UTAS Programming Club\n"
+       "\n"
+       "Currently unimplemented :(");
+  getchar();
+  resetconsole();
   return 0;
 }
