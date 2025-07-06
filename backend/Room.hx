2eeec082080323fb2d83be84abe88ca75956597d
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2025-07-07 00:50:44 +1000

    Readd map file
    This should be linked to an ingame item eventually.

--- backend/Room.hx
+++ backend/Room.hx
@@ -1,20 +1,26 @@
 package backend;
 
 import haxe.Constraints;
+using StringTools;
+import sys.io.File;
+import sys.io.FileOutput;
 
 import backend.Action;
 // TODO: Fix backend depending on coregame extension?
 import backend.coregame.Actions;
+import backend.coregame.Rooms;
 import backend.coregame.Screens;
 import backend.Game;
 import backend.GameInfo;
 import backend.Screen;
 
 abstract class Room extends ActionScreen {
-  function getRoomBody(state: Game): UnicodeString return "";
+  function getRoomBody(state: Game): UnicodeString return '';
 
   // TODO: Move all the strings to extensions/campaigns
   function getBody(state: Game): UnicodeString {
+    writeMap(state);
+
     final x: Int = state.player.x;
     final y: Int = state.player.y;
     final room: GameRoom = state.campaign.rooms[x][y];
@@ -30,16 +36,16 @@ abstract class Room extends ActionScreen {
   // TODO: Move all the actions to extensions/campaigns
   function getAllActions(): Array<Action> {
     final actions: Array<Action> = [
-      new GoNorth("Go North"),
-      new GoEast("Go East"),
-      new GoSouth("Go South"),
-      new GoWest("Go West"),
+      new GoNorth('Go North'),
+      new GoEast('Go East'),
+      new GoSouth('Go South'),
+      new GoWest('Go West'),
       // getRoomActions goes here
-      new GotoScreen(PlayerEquipmentScreen, "Check Inventory"),
+      new GotoScreen(PlayerEquipmentScreen, 'Check Inventory'),
 #if testrooms
-      new Quit("Quit Game")
+      new Quit('Quit Game')
 #else
-      new GotoScreen(MainMenuScreen, "Return to main menu")
+      new GotoScreen(MainMenuScreen, 'Return to main menu')
 #end
     ];
 
@@ -49,6 +55,110 @@ abstract class Room extends ActionScreen {
 
     return actions;
   }
+
+
+  // Each room take 6x4 but the 6(?) required calls to writeMapRoom per room only
+  // handle the top left most 5x3 unless it is the right and/or bottom most room
+  static final RoomSizeX: Int = 6;
+  static final RoomSizeY: Int = 4;
+
+  static final XLine: UnicodeString = "─";
+  static final YLine: UnicodeString = "│";
+  static final UpperHalfYLine: UnicodeString = "╵";
+  static final LowerHalfYLine: UnicodeString = "╷";
+
+  static final TopRowChars: Array<UnicodeString> =    ['┌', '┬', '┐'];
+  static final MiddleRowChars: Array<UnicodeString> = ['├', '┼', '┤'];
+  static final BottomRowChars: Array<UnicodeString> = ['└', '┴', '┘'];
+
+  static function roomExists(state: Game, x: Int, y: Int): Bool {
+    final floorSize: Int = state.campaign.rooms.length;
+    if (x < 0 || y < 0 || x >= floorSize || y >= floorSize) {
+      return false;
+    }
+
+    return !(state.campaign.rooms[x][y] is UnusedRoom);
+  }
+
+  static function writeMapRoom(file: FileOutput, x: Int, y: Int, line: Int, state: Game) : Void {
+    if (line == RoomSizeY - 1 && y != 0) {
+      return;
+    }
+
+    // Top and bottom row lines
+    if (line == 0 || line == RoomSizeY - 1) {
+      var rowChars: Array<UnicodeString>;
+      if (y == state.campaign.rooms.length - 1) {
+        rowChars = TopRowChars;
+      } else if (y > 0 || line == 0) {
+        rowChars = MiddleRowChars;
+      } else {
+        rowChars = BottomRowChars;
+      }
+
+      if (x == 0) {
+        file.writeString(rowChars[0]);
+      } else {
+        file.writeString(rowChars[1]);
+      }
+
+      if (line == 0 && roomExists(state, x, y + 1)) {
+        file.writeString(XLine + ''.rpad(' ', RoomSizeX - 4) + XLine);
+      } else {
+        file.writeString(''.rpad(XLine, RoomSizeX - 2));
+      }
+
+      if (x == state.campaign.rooms.length - 1) {
+        file.writeString(rowChars[2] + '\n');
+      }
+
+    // middle row lines
+    } else {
+      final roomExists: Bool = roomExists(state, x, y);
+
+      var wallChar: UnicodeString;
+      if (!roomExists || !Room.roomExists(state, x - 1, y)) {
+        wallChar = YLine;
+      } else if (line == 1) {
+        wallChar = UpperHalfYLine;
+      } else if (line == RoomSizeY - 2) {
+        wallChar = LowerHalfYLine;
+      } else {
+        wallChar = ' ';
+      }
+
+      // The pads are all -1 to allow room for the wallChar on the right side
+      // Player in room
+      if (x == state.player.x && y == state.player.y && line == 1) {
+        file.writeString((wallChar + 'P').rpad(' ', RoomSizeX - 1));
+      // Room exists
+      } else if (roomExists) {
+        file.writeString(wallChar.rpad(' ', RoomSizeX - 1));
+      // Room does not exists
+      } else {
+        file.writeString((wallChar + 'NO').rpad(' ', RoomSizeX - 1));
+      }
+
+      if (x == state.campaign.rooms.length - 1) {
+        file.writeString(YLine + '\n');
+      }
+    }
+  }
+
+  static function writeMap(state: Game): Void {
+    final file: FileOutput = File.write('map.txt', false);
+
+    for (flippedY in 0...state.campaign.rooms.length) {
+      final y = state.campaign.rooms.length - flippedY - 1;
+      for (line in 0...RoomSizeY) {
+        for (x in 0...state.campaign.rooms.length) {
+          writeMapRoom(file, x, y, line, state);
+        }
+      }
+    }
+
+    file.close();
+  }
 }
 
 // Keep in sync with StatefulActionScreen in Screen.hx
@@ -57,7 +167,7 @@ abstract class StatefulRoom<T : ScreenState & Constructible<Void -> Void>> exten
   override function hasState(): Bool return true;
   override function createState(): T return new T();
 
-  function getStatefulRoomBody(state: Game, screenState: T): UnicodeString return "";
+  function getStatefulRoomBody(state: Game, screenState: T): UnicodeString return '';
 
   override function getRoomBody(state: Game): UnicodeString return getStatefulRoomBody(state, state.getRoomState());
 }
