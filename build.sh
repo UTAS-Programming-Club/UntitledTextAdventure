defc4fd2b06dee4415915b030b9ede74e895d8d8
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2026-08-16 14:59:25 +1000

    Tidy up build scripts

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
-set -e
+set -eu
 
 # Save for release builds, causes clang to ignore printf argument related warnings
 # -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3
 
 CC="${CC:-cc}"
-
-mkdir -p bin/
-$CC -O3 -Wall -Wformat -Wformat=2 -Wconversion -Wimplicit-fallthrough \
+CFLAGS="-O3 -Wall -Wformat -Wformat=2 -Wconversion -Wimplicit-fallthrough \
 -Werror=format-security \
 -D_GLIBCXX_ASSERTIONS \
 -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_FAST \
@@ -20,27 +18,18 @@ $CC -O3 -Wall -Wformat -Wformat=2 -Wconversion -Wimplicit-fallthrough \
 -fcf-protection=full \
 -fno-delete-null-pointer-checks -fno-strict-overflow -fno-strict-aliasing -ftrivial-auto-var-init=zero \
 -Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion \
--std=c23 -pedantic -I src \
-$UTA_C_FLAGS \
+-pedantic -I src/"
+
+mkdir -p bin/
+# shellcheck disable=SC2086
+"$CC" -std=c23 $CFLAGS $UTA_C_FLAGS \
 src/dsl/dsl.c -o bin/utatest2026-dsl
 
 mkdir -p gen/
 ./bin/utatest2026-dsl src/coregame.uta gen/coregame.h gen/coregame.c
 ./bin/utatest2026-dsl src/ext1.uta gen/ext1.h gen/ext1.c
 
-$CC -O3 -Wall -Wformat -Wformat=2 -Wconversion -Wimplicit-fallthrough \
--Werror=format-security \
--D_GLIBCXX_ASSERTIONS \
--D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_FAST \
--fstrict-flex-arrays=3 \
--fstack-clash-protection -fstack-protector-strong \
--Wl,-z,nodlopen -Wl,-z,noexecstack \
--Wl,-z,relro -Wl,-z,now \
--Wl,--as-needed -Wl,--no-copy-dt-needed-entries \
--fPIE -pie \
--fcf-protection=full \
--fno-delete-null-pointer-checks -fno-strict-overflow -fno-strict-aliasing -ftrivial-auto-var-init=zero \
--Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion \
--std=c23 -pedantic -I . -I src/ \
-$UTA_C_FLAGS \
+
+# shellcheck disable=SC2086
+"$CC" -std=c23 -I . $CFLAGS $UTA_C_FLAGS \
 src/frontend.c src/backend.c src/coregame2.c gen/coregame.c gen/ext1.c -o bin/utatest2026
--- header_check.sh
+++ header_check.sh
@@ -1,55 +1,43 @@
 #!/usr/bin/env sh
-set -e
+set -eu
+
+IWYU="${IWYU:-../C/include-what-you-use/build/bin/include-what-you-use}"
+
+CFLAGS="-O3 -Wall -Wformat -Wformat=2 -Wconversion -Wimplicit-fallthrough \
+-Werror=format-security \
+-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3 \
+-D_GLIBCXX_ASSERTIONS \
+-D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_FAST \
+-fstrict-flex-arrays=3 \
+-fstack-clash-protection -fstack-protector-strong \
+-Wl,-z,nodlopen -Wl,-z,noexecstack \
+-Wl,-z,relro -Wl,-z,now \
+-Wl,--as-needed -Wl,--no-copy-dt-needed-entries \
+-fPIE -pie \
+-fcf-protection=full \
+-fno-delete-null-pointer-checks -fno-strict-overflow -fno-strict-aliasing -ftrivial-auto-var-init=zero \
+-Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion \
+-pedantic -I src/ \
+-Xiwyu --update_comments -Xiwyu --max_line_length=250"
 
 test_dsl_src()
 (
-  ../C/include-what-you-use/build/bin/include-what-you-use -O3 -Wall -Wformat -Wformat=2 -Wconversion -Wimplicit-fallthrough \
-  -Werror=format-security \
-  -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3 \
-  -D_GLIBCXX_ASSERTIONS \
-  -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_FAST \
-  -fstrict-flex-arrays=3 \
-  -fstack-clash-protection -fstack-protector-strong \
-  -Wl,-z,nodlopen -Wl,-z,noexecstack \
-  -Wl,-z,relro -Wl,-z,now \
-  -Wl,--as-needed -Wl,--no-copy-dt-needed-entries \
-  -fPIE -pie \
-  -fcf-protection=full \
-  -fno-delete-null-pointer-checks -fno-strict-overflow -fno-strict-aliasing -ftrivial-auto-var-init=zero \
-  -Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion \
-  -std=c23 -pedantic -I src \
-  $UTA_C_FLAGS \
-  -Xiwyu --update_comments -Xiwyu --max_line_length=250 \
-  $1
+  # shellcheck disable=SC2086
+  "$IWYU" -std=c23 $CFLAGS $UTA_C_FLAGS "$1"
 )
 
 test_game_src()
 (
-  ../C/include-what-you-use/build/bin/include-what-you-use -O3 -Wall -Wformat -Wformat=2 -Wconversion -Wimplicit-fallthrough \
-  -Werror=format-security \
-  -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3 \
-  -D_GLIBCXX_ASSERTIONS \
-  -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_FAST \
-  -fstrict-flex-arrays=3 \
-  -fstack-clash-protection -fstack-protector-strong \
-  -Wl,-z,nodlopen -Wl,-z,noexecstack \
-  -Wl,-z,relro -Wl,-z,now \
-  -Wl,--as-needed -Wl,--no-copy-dt-needed-entries \
-  -fPIE -pie \
-  -fcf-protection=full \
-  -fno-delete-null-pointer-checks -fno-strict-overflow -fno-strict-aliasing -ftrivial-auto-var-init=zero \
-  -Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion \
-  --std=c99 -pedantic -I . -I src/ \
-  $UTA_C_FLAGS \
-  -Xiwyu --update_comments -Xiwyu --max_line_length=250 \
-  $1
+  # shellcheck disable=SC2086
+  "$IWYU" --std=c99 -I . $CFLAGS $UTA_C_FLAGS "$1"
 )
 
 test_dsl_src src/dsl/dsl.c
-read IGNORE
+read -r IGNORE
 
 test_game_src src/backend.c
-read IGNORE
+read -r IGNORE
 test_game_src src/coregame2.c
-read IGNORE
+# shellcheck disable=SC2034
+read -r IGNORE
 test_game_src src/frontend.c
